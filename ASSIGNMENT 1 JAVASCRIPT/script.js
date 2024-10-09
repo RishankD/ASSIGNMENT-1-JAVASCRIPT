1ff6b4509a3163f4ecd51c9a9bb0cbba57dcfaf5
RishankD <156447323+RishankD@users.noreply.github.com>
2024-10-08 23:04:05 -0400

    Update script.js

---
+++ ASSIGNMENT 1 JAVASCRIPT/script.js
@@ -0,0 +1,5 @@
+const words1 = ["The cat", "A dog", "My friend", "An alien"];
+const words2 = ["jumps over", "runs to", "flies above", "sneaks around"];
+const words3 = ["the moon", "a car", "the house", "the tree"];
+const words4 = ["quickly", "slowly", "mysteriously", "excitedly"];
+const words5 = ["and disappears.", "and sleeps.", "and smiles.", "and dances."];
